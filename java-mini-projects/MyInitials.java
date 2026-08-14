8ca466e721eaf645d44ec5714d802df1001d9e52
coachakin <olaniyanmutiullahiakinkunmicoa@gmail.com>
2026-08-14 10:08:14 +0100

    printing my initials

---
+++ java-mini-projects/MyInitials.java
@@ -0,0 +1,29 @@
+// my intials print out
+
+public class MyInitials 
+{
+    public static void main (String [] args)
+    {
+        System.out.println (  """    
+
+                                
+                               
+                               *****         ****    ****           **
+                             **     **       ** **  ** **          ****
+                            **       **      **  ****  **         **  **
+                            **       **      **        **        ** ** **
+                             **     **       **        **       **      **
+                              **   **        **        **      **        **
+                                ***          **        **     **          **
+                                
+                                 
+                            """
+                            );            
+                                    
+
+
+
+  }   
+
+
+}
